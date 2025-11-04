023f905040c19e485150f5c300e878edf30a7d6c
Kawan <kawan.brito@ges.inatel.br>
2025-11-04 00:08:57 -0300

    8 mim so ajuda eu

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://automationexercise.com', 
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+    },
+  },
+
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/reports',
     overwrite: false,
     html: false,
     json: true
-  },
+  }
 });
